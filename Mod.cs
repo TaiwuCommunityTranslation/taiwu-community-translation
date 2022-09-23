8fee4d682af9f5ff03782d315324140c595bfbb2
Dmitri Roujan <dmitri@bandannabandit.games>
2022-09-22 18:41:56 -0700

    Converted to the Harmony variant so we can do method patching. Moved bottom buttons fix to harmony method so that the GameObjects are active when it's called. Added a log file to the game folder for easier access. Added auto text adjustment to TMPro elements.

--- Mod.cs
+++ Mod.cs
@@ -5,14 +5,21 @@ using DG.Tweening;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+using HarmonyLib;
 
 namespace TaiwuCommunityTranslation
 {
     [PluginConfig("Taiwu Community Translation", "Taiwu Mods Community", "0.1.0")]
-    public class Mod : TaiwuRemakePlugin  
+    public class Mod : TaiwuRemakeHarmonyPlugin
     {
         public override void Initialize()
         {
+            base.Initialize();
+
+            Application.logMessageReceived += Log;
             Debug.Log("!!!!! Taiwu Community Translation loaded");
 
             var prefix = @"Languages\en";
@@ -29,19 +36,6 @@ namespace TaiwuCommunityTranslation
             uiLanguageField.SetValue(null, lines);
 
             Debug.Log("!!!!! cleared LocalStringManager._localUILanguageArray");
-
-            // fix bottom buttons width
-            //var bbl = GameObject.Find("/Camera_UIRoot/Canvas/LayerMain/UI_MainMenu/FrontElements/BottomBtnLayout");
-            //var bblChildrenTransforms = bbl.GetComponentsInChildren<RectTransform>();
-            //foreach (var transform in bblChildrenTransforms)
-            //{
-            //    if (transform.gameObject.name == "LabelRoot")
-            //    {
-            //        var sizeDelta = transform.sizeDelta;
-            //        sizeDelta.x = 90;
-            //        transform.sizeDelta = sizeDelta;
-            //    }
-            //}
 
             Debug.Log("??????");
 
@@ -93,11 +87,76 @@ namespace TaiwuCommunityTranslation
                     sequence.Play();
                 }
             });
+
+            TranslatorAssistant.AddToGame();
         }
 
         public override void Dispose()
         {
             // Nothing
+            Application.logMessageReceived -= Log;
+        }
+
+        public void Log(string logString, string stackTrace, LogType type)
+        {
+            string filename = "EngModLogs.txt";
+
+            try
+            {
+                System.IO.File.AppendAllText(filename, logString + "\n");
+            }
+            catch { }
+        }
+    }
+
+
+    public class TranslatorAssistant : MonoBehaviour
+    {
+        public bool enableAutoSizing = true;
+        public static void AddToGame()
+        {
+            GameObject go = new GameObject();
+            go.name = "Translator Assistant";
+            DontDestroyOnLoad(go);
+            go.AddComponent<TranslatorAssistant>();
+            Debug.Log("Translator assistant successfully added to game.");
+        }
+
+        void Update()
+        {
+            System.Array.ForEach(FindObjectsOfType<TextMeshProUGUI>(), AdjustTMPro);
+        }
+
+        void AdjustTMPro(TextMeshProUGUI textMesh)
+        {
+            if (enableAutoSizing)
+            { 
+                textMesh.fontSizeMin = 12;
+                textMesh.fontSizeMax = 24;
+                textMesh.enableAutoSizing = true;
+            }
+            
+        }
+    }
+
+    [HarmonyPatch(typeof(UI_MainMenu), nameof(UI_MainMenu.OnInit))]
+    class MainMenuPatch
+    {
+        static void Postfix()
+        {
+            // fix bottom buttons width
+            Debug.Log("Main Menu harmony patch");
+            GameObject bbl = GameObject.Find("/Camera_UIRoot/Canvas/LayerMain/UI_MainMenu/FrontElements/BottomBtnLayout");
+            RectTransform[] bblChildrenTransforms = bbl.GetComponentsInChildren<RectTransform>();
+
+            foreach (var transform in bblChildrenTransforms)
+            {
+                if (transform.gameObject.name == "LabelRoot")
+                {
+                    Vector2 sizeDelta = transform.sizeDelta;
+                    transform.sizeDelta = new Vector2(90, sizeDelta.y);
+                }
+            }
         }
     }
 }
