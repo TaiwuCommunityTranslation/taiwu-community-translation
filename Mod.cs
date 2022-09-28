249cc10518309ff3e26995b89f9fe0e69b59f6ea
Dmitri Roujan <dmitri@bandannabandit.games>
2022-09-27 17:16:52 -0700

    Event Translation

--- Mod.cs
+++ Mod.cs
@@ -1,20 +1,18 @@
-﻿using System.Reflection;
+﻿using Config;
+using Config.Common;
+using FrameWork;
+using HarmonyLib;
+using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using TaiwuModdingLib.Core.Plugin;
 using TMPro;
 using UnityEngine;
-using HarmonyLib;
-using Newtonsoft.Json;
-using System.Collections.Generic;
-using System.Linq;
-using System.Collections;
-using System.Threading.Tasks;
-using System;
-using Config.Common;
-using Config;
-using FrameWork;
-using FrameWork.AssetBundlePackage;
-using FrameWork.ResManager;
 
 namespace TaiwuCommunityTranslation
 {
@@ -23,6 +21,7 @@ namespace TaiwuCommunityTranslation
     {
         public static readonly string prefix = @"Languages\en";
         public static bool enableAutoSizing = true;
+
         public override void Initialize()
         {
             base.Initialize();
@@ -30,8 +29,14 @@ namespace TaiwuCommunityTranslation
             Application.logMessageReceived += Log;
             Debug.Log("!!!!! Taiwu Community Translation loaded");
             if (!Directory.Exists(prefix)) return;
+            TranslateEvents();
             TranslatorAssistant.AddToGame();
-            
+        }
+
+        public override void OnModSettingUpdate()
+        {
+            base.OnModSettingUpdate();
+            Debug.Log("Hello World");
         }
 
         public override void Dispose()
@@ -51,7 +56,209 @@ namespace TaiwuCommunityTranslation
             catch { }
         }
 
+        public bool generateEvent = false;
+        public readonly string EventDir = @"Event/EventLanguages";
+        public void TranslateEvents()
+        {
+            Debug.Log("Translating Events");
+            DirectoryInfo d = new DirectoryInfo(EventDir); //Assuming Test is your Folder
+            Debug.Log(d.FullName);
+            Dictionary<string, FileInfo> files = d.GetFiles("*.txt").ToDictionary(file => file.Name); //Getting Text files
+            Dictionary<string, TaiWuTemplate> parsedTemplates = files.ToDictionary(f => f.Key, f => new TaiWuTemplate(f.Value));
 
+            //For dev purposes
+            if (generateEvent)
+            {
+                Directory.CreateDirectory(Path.Combine(EventDir, "EventDump"));
+                Dictionary<string, string> flatDict = parsedTemplates.Values
+                    .ToList()
+                    .SelectMany(template => template.FlattenTemplateToDict())
+                    .ToDictionary(pair => pair.Key, pair => pair.Value);
+                File.WriteAllText(Path.Combine(EventDir, "EventDump") + "/events.json", JsonConvert.SerializeObject(flatDict, Formatting.Indented));
+            }
+            else
+            {
+                //Write to new files
+                var file = @"events.json";
+                var dict = JsonConvert.DeserializeObject<Dictionary<string, string>>(File.ReadAllText(Path.Combine(Mod.prefix, file)));
+
+                // Apply values to files in memory
+                dict.Keys.ToList().ForEach(key =>
+                {
+
+                    string[] parsedKey = key.Split('|');
+                    if (parsedKey.Length != 3 && parsedKey.Length != 4) Debug.LogError($"A key has been incorrectly parsed:{parsedKey} ");
+
+                    string val = dict[key];
+                    if (val == "") return;
+
+
+                    string fileName = parsedKey[0];
+                    string guid = parsedKey[1];
+                    string templateKey = parsedKey[2];
+                    int parsedIndex = -1;
+
+                    Debug.Log(key);
+                    if (parsedKey.Length == 4) parsedIndex = int.Parse(parsedKey[3]);
+
+                    parsedTemplates[fileName].eventMap[guid].ApplyValue(templateKey, val, parsedIndex);
+                });
+
+                Debug.Log("Starting file write");
+                parsedTemplates.Keys.ToList().ForEach(key => {
+                    parsedTemplates[key].WriteBackToFile();
+                });
+            }
+
+        }
+
+        public class TaiWuTemplate {
+
+            string fileName = "";
+            public List<EventData> eventData = new List<EventData>();
+            public Dictionary<string, EventData> eventMap = new Dictionary<string, EventData>();
+
+            FileInfo file;
+            public TaiWuTemplate(FileInfo file)
+            {
+                EventData activeEventData = null;
+                fileName = file.Name;
+                this.file = file;
+                Debug.Log($"Generating Template For {fileName}");
+                StreamReader textStream = file.OpenText();
+                while (!textStream.EndOfStream)
+                {
+                    string line = textStream.ReadLine();
+                    if (line.Length == 0) continue;
+                    int index = line.IndexOf(':');
+                    string key = line.Substring(0, index);
+                    string value = line.Substring(index+2); //Skip the ':' and the whitespace
+
+                    switch(new string(key.Where(Char.IsLetter).ToArray()))
+                    {
+                        case "EventGuid":
+                            if (activeEventData != null)
+                            {
+                                eventData.Add(activeEventData);
+                            }
+                            activeEventData = new EventData() { guid = value };
+                            break;
+                        case "EventContent":
+                            activeEventData.content = value;
+                            break;
+                        case "Option":
+                            activeEventData.options.Add(value);
+                            break;
+                    }
+                }
+                textStream.Close();
+                //Adds the last one
+                eventData.Add(activeEventData);
+
+                eventMap = eventData.ToDictionary(eventData => eventData.guid);
+            }
+
+            public void WriteBackToFile()
+            {
+                StringBuilder sb = new StringBuilder();
+                StreamReader textStream = file.OpenText();
+                EventData activeEventData = null;
+                while (!textStream.EndOfStream)
+                {
+                    string line = textStream.ReadLine();
+                    string lineToWrite = line;
+
+                    if (line.Length == 0)
+                    {
+                        sb.AppendLine();
+                        continue;
+                    }
+
+                    int index = line.IndexOf(':');
+                    string key = line.Substring(0, index);
+                    string value = line.Substring(index + 2); //Skip the ':' and the whitespace
+
+                    switch (new string(key.Where(Char.IsLetter).ToArray()))
+                    {
+                        case "EventGuid":
+                            activeEventData = eventMap[value];
+                            break;
+                        case "EventContent":
+                            lineToWrite = activeEventData.GenerateContentString();
+                            break;
+                        case "Option":
+                            lineToWrite = activeEventData.GenerateOptString(new string(key.Where(Char.IsDigit).ToArray()));
+                            break;
+                    }
+                    if(lineToWrite != "")
+                    {
+                        sb.AppendLine(lineToWrite);
+                    }
+                    else
+                    {
+                        sb.AppendLine(line);
+                    }
+                }
+                textStream.Close();
+                File.WriteAllText(file.FullName, sb.ToString());
+            }
+
+
+            public Dictionary<string, string> FlattenTemplateToDict()
+            {
+                Dictionary<string, string> dict = new Dictionary<string, string>();
+                eventData.ForEach(x =>
+                {
+                    string baseString = $"{fileName}|{x.guid}";
+                    if (x.content != null || x.content != "" || x.content != " ") dict.Add($"{baseString}|EventContent", x.content);
+                    for (int i = 0; i < x.options.Count; i++)
+                    {
+                        dict.Add($"{baseString}|Option|{i}", x.options[i]);
+                    }
+                });
+                return dict;
+            }
+        }
+
+        public class EventData
+        {
+            public string guid;
+            public string content = "";
+            public List<string> options = new List<string>();
+
+            public void ApplyValue(string templateKey, string val, int index = -1)
+            {
+                if (val == "") return;
+                if(templateKey.Contains("Option") && index > -1)
+                {
+                    options[index] = " "+val;
+                    return;
+                }
+                else if(templateKey == "EventContent")
+                {
+                    content = " "+val;
+                    return;
+                }
+                Debug.LogError($"Ya fucked up son, ${guid} | {templateKey} , index: {index}");
+            }
+
+            public string GenerateOptString(string i)
+            {
+                string val = options[int.Parse(i)-1];
+                if (val == "") return "";
+
+
+                return $"		-- Option_{i} :{val}";
+            }
+
+            public string GenerateContentString()
+            {
+                if (content == "") return "";
+
+
+                return $"		-- EventContent :{content}";
+            }
+        }
     }
 
     public class TranslatorAssistant : MonoBehaviour
@@ -115,7 +322,6 @@ namespace TaiwuCommunityTranslation
             foreach (var entry in dict)
             {
                 if (entry.Value == "") continue;
-                Debug.Log($"'{entry.Key},{entry.Value}'");
                 var id = LanguageKey.LanguageKeyToId(entry.Key);
                 LocalStringManager._localUILanguageArray[id] = entry.Value;
             }
